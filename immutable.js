1742ffb6a065adaae75b3bab46dc5bb5361f70a0
Tyge <tygesjohnson@gmail.com>
2018-02-20 14:40:19 -0700

    obj assignment

--- immutable.js
+++ immutable.js
@@ -8,12 +8,23 @@
 */
 
 /* Problem 1
-  Create an object called `person`. 
+  Create an object called `person`.
   It should have a name property, age and hometown.
   Create a `newPerson` object from `person` using Object.assign.
   Lets say that `newPerson` has the same hometown, but different name and age.
   Be sure to console.log() both references to ensure that they are pointing to different objects.
 */
+
+const person = {
+  name: 'Joe',
+  age: 20,
+  hometown: 'SLC'
+}
+
+let newPerson = Object.assign({}, person);
+
+newPerson.name = 'sally';
+newPerson.age = 45;
 
 console.log(person);
 console.log(newPerson);
@@ -31,7 +42,7 @@ console.log(combinedSupers);
 /* Problem 3
   Create an object `studentScores` with a 'name', 'age' and 'scores'
   Where 'name' is a string, 'age' is a number and 'scores' is an array of numbers
-  The numbers for scores should be between 1 - 50. 
+  The numbers for scores should be between 1 - 50.
   Create a new object `studentAverage` using Object.assign() from the given original object.
   The new object should filter out any scores that are < 34 Array.filter should be used here.
 */
